b5ac40b72f4dd6b1a79cf68ee0bffaf306df8a47
Mauricio Roberto <mauricio.roberto.dev@gmail.com>
2026-04-09 22:01:25 -0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ export * from "@/errors.js";
 export type { Order } from "@/generated/server.js";
 export type { BallObject } from "@/interfaces/ball.interface.js";
 export type { IBot } from "@/interfaces/bot.interface.js";
+export type { EnvironmentObject } from "@/interfaces/environment.interface.js";
 export type { FieldZoneObject } from "@/interfaces/field-zone.interface.js";
 export { type FormationObject, FormationType } from "@/interfaces/formation.interface.js";
 export type { GameInspectorObject, IGameInspector } from "@/interfaces/inspector.interface.js";
